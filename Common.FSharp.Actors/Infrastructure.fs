888191fdce526afab08d3edcc92bedc713230efe
psgivens <psgivens11@yahoo.com>
2019-04-27 16:41:46 -0700

    Moving Common.FSharp into its own repository.

--- Common.FSharp.Actors/Infrastructure.fs
+++
@@ -1,7 +0,0 @@
-﻿module Common.FSharp.Actors.Infrastructure
-
-open Akka.Actor
-open Akka.FSharp
-open Common.FSharp.Envelopes
-
-type ActorIO<'a> = { Tell:Envelope<'a> -> unit; Actor:IActorRef; Events:IActorRef; Errors:IActorRef }
